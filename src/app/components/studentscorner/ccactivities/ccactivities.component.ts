9d1a59ba9ab2e5c2cd53d74193d38c1f9ce24418
Rajeevhr10 <rajeevhr10@gmail.com>
2024-07-15 00:12:11 +0530

    Added facilities component

--- src/app/components/studentscorner/ccactivities/ccactivities.component.ts
+++ src/app/components/studentscorner/ccactivities/ccactivities.component.ts
@@ -3,8 +3,67 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-ccactivities',
   templateUrl: './ccactivities.component.html',
-  styleUrls: ['./ccactivities.component.css']
+  styleUrls: ['./ccactivities.component.css'],
 })
 export class CcactivitiesComponent {
-
+  images = [
+    {
+      name: 'Laboratory 1',
+      image:
+        'https://dl.dropboxusercontent.com/scl/fi/nk66mxkyif1tme692cf0j/cc-2.JPG?rlkey=ykqi30bu9fnb88uy2brkbkx24&st=olbdisqi&dl=0',
+      description:
+        'The laboratories are completely equipped with items required as per norms...',
+    },
+    {
+      name: 'Laboratory 2',
+      image:
+        'https://dl.dropboxusercontent.com/scl/fi/itdayhi6gdoyrcdfkrlcm/cc-1.JPG?rlkey=hus5z3ur1kfwez0tp4h5boi14&st=4dkmf10t&dl=0',
+      description:
+        'The laboratories are constructed with keeping in mind their compatibility...',
+    },
+    {
+      name: 'Laboratory 1',
+      image:
+        'https://dl.dropboxusercontent.com/scl/fi/7p4u0f3w33s6hl60pwv4y/cc4.jpg?rlkey=76896scaw962bk2q3vttdr27f&st=12olsxkp&dl=0',
+      description:
+        'The laboratories are completely equipped with items required as per norms...',
+    },
+    {
+      name: 'Laboratory 2',
+      image:
+        'https://dl.dropboxusercontent.com/scl/fi/5gafmyufv946wbnlzkvdu/cc5.jpg?rlkey=dfm7dm48t1axcmmzl1xh7urfa&st=d5z93mtb&dl=0',
+      description:
+        'The laboratories are constructed with keeping in mind their compatibility...',
+    },
+    {
+      name: 'Laboratory 1',
+      image:
+        'https://dl.dropboxusercontent.com/scl/fi/sjxf7plrxzh33ayktrjxd/cc6.jpg?rlkey=qpc53tka04fi6r0tvagk92zym&st=lzxznpq5&dl=0',
+      description:
+        'The laboratories are completely equipped with items required as per norms...',
+    },
+    {
+      name: 'Laboratory 2',
+      image:
+        'https://dl.dropboxusercontent.com/scl/fi/sjxf7plrxzh33ayktrjxd/cc6.jpg?rlkey=qpc53tka04fi6r0tvagk92zym&st=935aj0m3&dl=0',
+      description:
+        'The laboratories are constructed with keeping in mind their compatibility...',
+    },
+    {
+      name: 'Laboratory 1',
+      image:
+        'https://dl.dropboxusercontent.com/scl/fi/d2hqqayyvrb705slqgzme/cc7.jpg?rlkey=wgyqg0511cwnzeem6m6h6e5t4&st=q0oe4eeh&dl=0',
+      description:
+        'The laboratories are completely equipped with items required as per norms...',
+    },
+    {
+      name: 'Laboratory 2',
+      image:
+        'https://dl.dropboxusercontent.com/scl/fi/rt329oyawvmuhwzdc4n6w/cc8.jpg?rlkey=m1edscjggznqgdeogrcktp6xs&st=toq9sqcb&dl=0',
+      description:
+        'The laboratories are constructed with keeping in mind their compatibility...',
+    },
+    
+    // Add more labs as needed
+  ];
 }
